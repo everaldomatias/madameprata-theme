1304fea8a433c267bc61751b4fb8ca83d74d46f6
everaldomatias <matiaseveraldo@gmail.com>
2022-04-26 20:52:52 -0300

    Hide other shipping method when free is enabled

--- functions.php
+++ functions.php
@@ -48,3 +48,22 @@ function mp_home_search() {
 
 }
 add_action( 'mfn_hook_content_before', 'mp_home_search' );
+
+/**
+ * Hide shipping rates when free shipping is available.
+ * Updated to support WooCommerce 2.6 Shipping Zones.
+ *
+ * @param array $rates Array of rates found for the package.
+ * @return array
+ */
+function mp_hide_shipping_when_free_is_available( $rates, $package ) {
+	$free = array();
+	foreach ( $rates as $rate_id => $rate ) {
+		if ( 'free_shipping' === $rate->method_id ) {
+			$free[ $rate_id ] = $rate;
+			break;
+		}
+	}
+	return ! empty( $free ) ? $free : $rates;
+}
+add_filter( 'woocommerce_package_rates', 'mp_hide_shipping_when_free_is_available', 100, 2 );
