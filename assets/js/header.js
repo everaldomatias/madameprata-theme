959ffc9233be69c625a95220a4c867be54f23298
Everaldo Matias <matiaseveraldo@gmail.com>
2023-08-24 20:44:16 -0300

    Fix error on call header element in js file

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     let header = document.querySelector('#Action_bar .column,one');
+	if (header) {
+		const content = "Compre com segurança<br>Qualidade da Prata 925";
 
-    const content = "Compre com segurança<br>Qualidade da Prata 925";
-
-    let element = document.createElement("div");
-    element.classList.add('selo');
-    element.innerHTML = content;
-    header.appendChild(element);
+		let element = document.createElement("div");
+		element.classList.add('selo');
+		element.innerHTML = content;
+		header.appendChild(element);
+	}
 })
